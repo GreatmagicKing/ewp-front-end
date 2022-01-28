f8c2ab879715e3ade9c1b3095cd56d6933b0edc1
zhang，jiajun <jiajun.b.zhang@accenture.com>
2022-01-28 18:45:55 +0800

    使用List组件

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,7 +4,7 @@ import { Col, Menu, Row } from 'antd';
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import styles from './header.module.css';
 import EmployeesHome from '../../pages/EmployeesHome/EmployeesHome';
-import { Link, Route } from 'react-router-dom';
+import { Link, Redirect, Route } from 'react-router-dom';
 import LearnHub from '../../pages/LearnHub/LearnHub';
 import Career from '../../pages/Career/Career';
 import WorkCenter from '../../pages/WorkCenter/WorkCenter';
@@ -12,16 +12,20 @@ const { SubMenu } = Menu;
 
 export default class Header extends Component {
     state = {
-        current: 'mail',
+        current: 'LearnHub',
     };
 
     handleClick = (e: any) => {
         console.log('click ', e);
         this.setState({ current: e.key });
+        
     };
+    componentDidMount() {
 
+    }
     render() {
         const { current } = this.state;
+        
         return (
             <div>
                 <Row>
@@ -43,19 +47,10 @@ export default class Header extends Component {
                             <Menu.Item key="WorkCenter" className={styles.headerTitleFontSize} >
                                 <Link to="/WorkCenter">Work Center</Link>
                             </Menu.Item>
-                            {/* <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-                                <Menu.ItemGroup title="Item 1">
-                                    <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                    <Menu.Item key="setting:2">Option 2</Menu.Item>
-                                </Menu.ItemGroup>
-                                <Menu.ItemGroup title="Item 2">
-                                    <Menu.Item key="setting:3">Option 3</Menu.Item>
-                                    <Menu.Item key="setting:4">Option 4</Menu.Item>
-                                </Menu.ItemGroup>
-                            </SubMenu> */}
                         </Menu>
                     </Col>
                 </Row>
+                <Route exact path="/" component={LearnHub}/>
                 <Route path="/LearnHub" component={LearnHub}/>
                 <Route path="/Career" component={Career}/>
                 <Route path="/WorkCenter" component={WorkCenter}/>
--- src/pages/LearnHub/LearnHub.tsx
+++ src/pages/LearnHub/LearnHub.tsx
@@ -1,16 +1,61 @@
-import { Col, Row } from 'antd'
+import { Col, List, Row, Avatar } from 'antd'
 import React, { Component, Fragment } from 'react'
+import './LearnHub.css';
+
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+    {
+        title: 'Ant Design Title 5',
+    },
+    {
+        title: 'Ant Design Title 6',
+    },
+];
 export default class LearnHub extends Component {
     render() {
         return (
             <Fragment>
-                <div>
+                <div className='body'>
                     <Row>
                         <Col flex={1}></Col>
                         <Col flex={10}>
-                            <div>
-                                1111111111
-                            </div>
+                            <Row>
+                                <Col flex={2}>
+                                    <div className='infobox'>
+                                        <h3>学习社区</h3>
+                                        <List
+                                            itemLayout="vertical"
+                                            dataSource={data}
+                                            renderItem={item => (
+                                                <List.Item>
+                                                    <List.Item.Meta
+                                                        // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                                                        title={<a href="https://ant.design">{item.title}</a>}
+                                                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                                    />
+                                                    <div>1111111111111111111111111</div>
+                                                </List.Item>
+                                            )}
+                                        />
+                                    </div>
+                                </Col>
+                                <Col flex={1}>
+                                    <div className='infobox'>
+
+                                    </div>
+                                </Col>
+                            </Row>
                         </Col>
                         <Col flex={1}></Col>
                     </Row>
